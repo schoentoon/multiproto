5becc7f3008b1a2542e5cddb579fe34ea547ece2
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-06-22 01:07:59 +0200

    Added an irc matcher
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

---
+++ modules/src/irc.c
@@ -0,0 +1,28 @@
+/*  multiproto
+ *  Copyright (C) 2013  Toon Schoenmakers
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int matchData(unsigned char* data, size_t length) {
+  char* user = strstr((char*) data, "USER ");
+  char u0[65], u1[65], u2[65], u3[65];
+  if (user && sscanf(user, "USER %64s \"%64[^\"]\" \"%64[^\"]\" :%64s\r\n", u0, u1, u2, u3) == 4)
+    return 1;
+  return 0;
+}
