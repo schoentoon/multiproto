e9ea0a83ad47582bd3f8a5020860d8c47ce0126a
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-06-22 16:20:49 +0200

    Let's also support POST, PUT and DELETE in the http module
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- modules/src/http.c
+++ modules/src/http.c
@@ -25,7 +25,7 @@ int matchData(unsigned char* data, size_t length) {
   if (sscanf((char*) data, "%64s / HTTP/%d.%d\r\n", buf, &major, &minor) == 3
     || sscanf((char*) data, "%64s /%*s HTTP/%d.%d\r\n", buf, &major, &minor) == 3) {
     if (major >= 1 && minor >= 1) {
-      if (strcmp(buf, "GET") == 0)
+      if (strcmp(buf, "GET") == 0 || strcmp(buf, "POST") == 0 || strcmp(buf, "PUT") == 0 || strcmp(buf, "DELETE") == 0)
         return 1;
     }
   }
