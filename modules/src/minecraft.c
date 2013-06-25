b6f9a0227a6c375ea58c63e0c890cc569ab492e4
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-06-25 22:34:02 +0200

    Do a slightly more complete check
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- modules/src/minecraft.c
+++ modules/src/minecraft.c
@@ -15,13 +15,16 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "debug.h"
+
 #include <stdio.h>
+#include <ctype.h>
 
 /** I have no knowledge of how long this will keep working, but it is
  * based on a simple wireshark on a minecraft connection. Sort of looks
  * like this
 00000000  02 3d 00 0a 00 73 00 63  00 68 00 6f 00 65 00 6e .=...s.c .h.o.e.n
-00000010  00 74 00 6f 00 6f 00 6e  00 0d 00 31 00 39 00 32 .t.o.o.n ...1.9.2
+00000010  00 74 00 6f 00 6f 00 6e  00 1d 00 31 00 39 00 32 .t.o.o.n ...1.9.2
 00000020  00 2e 00 31 00 36 00 38  00 2e 00 32 00 2e 00 32 ...1.6.8 ...2...2
 00000030  00 30 00 30 00 00 63 dd                          .0.0..c. 
  * In which schoentoon is the username and 192.168.2.200 is the ip address of
@@ -34,8 +37,19 @@ int matchData(unsigned char* data, size_t length) {
       && data[1] == 0x3d
       && data[2] == 0x00
       && data[3] == 0x0a
-      && data[4] == 0x00)
+      && data[4] == 0x00) {
+      unsigned int i;
+      for (i = 5; i < length; i++) {
+        if (i % 2 == 1) {
+          if (!isalnum(data[i]))
+            return 0;
+        } else if (data[i] != 0x00)
+          return 0;
+        else if (data[i] == 0x00 && data[i+1] == 0x1d)
+          break;
+      }
       return 1;
+    }
   }
   return 0;
 }
